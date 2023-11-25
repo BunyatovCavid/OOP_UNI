1754c6c035460d3c72e500376634a078228511d1
bunyatovcavid2003@gmail.com <bunyatovcavid2003@gmail.com>
2023-11-25 20:50:21 +0400

    Add project files.

---
+++ OOP_UNI/Car.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_UNI
+{
+    internal class Car
+    {
+        //field
+        private int year;
+
+        public Car()
+        {
+            year = 2000;
+            Drive();
+        }
+
+
+        //Property
+        public int year2
+        { 
+            get
+            {
+                return year *2 ;
+            }
+            set
+            {
+                year2 = value;
+            }
+        }
+
+        public void Drive(string word, int number)
+        {
+
+        }
+        public void Drive(int number, string word)
+        {
+
+        }
+    }
+
+
+    abstract class Animal
+    {
+        public virtual string voice { get; set; }
+        public virtual string favoritefood { get; set; }
+        public virtual int year { get; set; }
+        public virtual string bloodtype { get; set; }
+        abstract protected int bonenumber { get; set; }
+
+
+        public virtual void Fly()
+        {
+
+        }
+
+        public abstract void Swim();
+
+        public void Run()
+        {
+
+        }
+    }
+
+    internal class Own : Animal
+    {
+        protected override int bonenumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string favoritefood { get => base.favoritefood; set => base.favoritefood = value; }
+        public override string voice { get => base.voice; set => base.voice = value; }
+
+        public override void Swim()
+        {
+           
+        }
+    }
+
+    internal class Vet : Animal
+    {
+
+        protected override int bonenumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string bloodtype { get => base.bloodtype; set => base.bloodtype = value; }
+
+        public override void Swim()
+        {
+            
+        }
+    }
+
+
+
+}
---
+++ OOP_UNI/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace OOP_UNI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car test = new Car();
+
+            test.Drive()
+
+            test.year2 = 2103;
+
+            Console.WriteLine(test.year2);
+
+            Console.ReadLine();
+
+            //Object Orianted Programing OOP
+
+            //Encapsulation
+
+            //Abstraction
+
+            //Inheritence
+
+            //Polyomrphizm
+
+        }
+    }
+}
